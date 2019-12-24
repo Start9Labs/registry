8915998a5f9f608e871ed177a82791b339e6f735
Aaron Greenspan <agreenspan@Salt-100014.local>
2019-12-23 19:00:03 -0700

    cleans semver

--- src/Lib/Resource.hs
+++ src/Lib/Resource.hs
@@ -2,13 +2,13 @@
 
 module Lib.Resource where
 
-import           Startlude         hiding (empty)
+import           Startlude         hiding (empty, toList)
 
-import           Data.Aeson
 import           Data.HashMap.Lazy hiding (mapMaybe)
 import           System.Directory
 import           System.FilePath
 
+import           Lib.Semver
 import           Lib.Types.Semver
 
 
@@ -26,6 +26,10 @@ s9pkFile appId = toS appId <.> "s9pk"
 
 type Registry = HashMap String (HashMap AppVersion FilePath)
 
+newtype RegisteredAppVersion = RegisteredAppVersion (AppVersion, FilePath)
+instance HasAppVersion RegisteredAppVersion where
+    version (RegisteredAppVersion (av, _)) = av
+
 loadResources :: MonadIO m => m Registry
 loadResources = liftIO $ do
     appDirectories <- getSubDirectories resourcePath
@@ -33,13 +37,14 @@ loadResources = liftIO $ do
         ( \hm appId -> do
             subdirs <- getSubDirectories (resourcePath </> appId)
             let validVersions = mapMaybe readMaybe subdirs
-            let newAppVersions = fromList $ fmap (, s9pkFile appId) validVersions
+            let newAppVersions = fromList $ fmap (\v -> (v, resourcePath </> show v </> s9pkFile appId)) validVersions
             pure $ insert appId newAppVersions hm
         ) empty appDirectories
     where
         getSubDirectories path = listDirectory path >>= filterM (fmap not . doesFileExist)
 
 getAppFile :: Registry -> String -> AppVersion -> Maybe FilePath
-getAppFile r appId av = do
-    s9pk <- lookup av <=< lookup appId $ r
-    pure $ resourcePath </> appId </> show av </> s9pk
+getAppFile r appId av = lookup av <=< lookup appId $ r
+
+registeredAppVersions :: Registry -> String -> [RegisteredAppVersion]
+registeredAppVersions r appId = fromMaybe [] $ fmap RegisteredAppVersion . toList <$> lookup appId r
