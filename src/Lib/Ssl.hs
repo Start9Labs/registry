b26ad51ba2ef3120a55f674bc0030fa607d7d84b
Keagan McClelland <keagan.mcclelland@gmail.com>
2020-08-04 15:11:48 -0600

    printing things for testing

--- src/Lib/Ssl.hs
+++ src/Lib/Ssl.hs
@@ -49,5 +49,7 @@ doesSslNeedRenew cert = do
 renewSslCerts :: FilePath -> ReaderT RegistryCtx IO ()
 renewSslCerts cert = do
     domain <- asks $ registryHostname . appSettings
+    liftIO $ putStrLn $ "DOMAIN: " <> domain
+    liftIO $ putStrLn $ "CERT: " <> cert
     void . liftIO $ system [i|certbot renew|]
     void . liftIO $ system [i|cp /etc/letsencrypt/live/#{domain}/fullchain.pem #{cert}|]
