a42c5c5caa253798e1d7eb5c2a618168f4c25aac
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2021-07-14 17:43:13 -0400

    eos endpoint, lowercase categories, sdk get instructions and license

--- src/Lib/Types/Category.hs
+++ src/Lib/Types/Category.hs
@@ -1,42 +1,42 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-
 module Lib.Types.Category where
 
-import Startlude 
+import Startlude
 import Database.Persist.Postgresql
 import Data.Aeson
 import Control.Monad
 import Yesod.Core
-
+import qualified Data.Text as T
 data CategoryTitle = FEATURED 
         | BITCOIN
         | LIGHTNING
         | DATA
         | MESSAGING
+        | SOCIAL
         | NONE
         | ANY
-    deriving (Eq, Show, Enum, Read)
+    deriving (Eq, Enum, Show, Read)
 instance PersistField CategoryTitle where
     fromPersistValue = fromPersistValueJSON
     toPersistValue = toPersistValueJSON
 instance PersistFieldSql CategoryTitle where
   sqlType _ = SqlString
 instance ToJSON CategoryTitle where
-    toJSON = String . show
+    toJSON = String . T.toLower . show
 instance FromJSON CategoryTitle where
     parseJSON = withText "CategoryTitle" $ \case
-        "FEATURED"   -> pure FEATURED
-        "BITCOIN"    -> pure BITCOIN
-        "LIGHTNING"  -> pure LIGHTNING
-        "DATA"       -> pure DATA
-        "MESSAGING"  -> pure MESSAGING
-        "NONE"       -> pure NONE
-        "ANY"        -> pure ANY
+        "featured"   -> pure FEATURED
+        "bitcoin"    -> pure BITCOIN
+        "lightning"  -> pure LIGHTNING
+        "data"       -> pure DATA
+        "messaging"  -> pure MESSAGING
+        "social"     -> pure SOCIAL
+        "none"       -> pure NONE
+        "any"        -> pure ANY
         _            -> fail "unknown category title"
 instance ToContent CategoryTitle where
     toContent = toContent . toJSON
 instance ToTypedContent CategoryTitle where
     toTypedContent = toTypedContent . toJSON
-
