f6758c39bdf73fa8119ce800d26a9233f67e3679
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2021-08-23 11:27:35 -0600

    rename endpoints

--- src/Handler/Icons.hs
+++ src/Handler/Icons.hs
@@ -20,6 +20,33 @@ getIconsR :: Extension "png" -> Handler TypedContent
 getIconsR ext = do
     -- @TODO switch to getting from service directory
     AppSettings{..} <- appSettings <$> getYesod
+    mPng <- liftIO $ getVersionedFileFromDir (resourcesDir </> "icons") ext
+    case mPng of
+        Nothing -> notFound
+        Just pngPath -> do
+            putStrLn @Text $ show pngPath
+            exists <- liftIO $ doesFileExist pngPath
+            if exists
+                then respondSource typePlain $ CB.sourceFile pngPath .| awaitForever sendChunkBS
+                else notFound
+
+getLicenseR :: Extension "" -> Handler TypedContent
+getLicenseR ext = do
+    AppSettings{..} <- appSettings <$> getYesod
+    mPng <- liftIO $ getUnversionedFileFromDir (resourcesDir </> "icons") ext
+    case mPng of
+        Nothing -> notFound
+        Just pngPath -> do
+            putStrLn @Text $ show pngPath
+            exists <- liftIO $ doesFileExist pngPath
+            if exists
+                then respondSource typePlain $ CB.sourceFile pngPath .| awaitForever sendChunkBS
+                else notFound
+
+getMarkdownR :: Extension "md" -> Handler TypedContent
+getMarkdownR ext = do
+    -- @TODO switch to getting from service directory
+    AppSettings{..} <- appSettings <$> getYesod
     mPng <- liftIO $ getUnversionedFileFromDir (resourcesDir </> "icons") ext
     case mPng of
         Nothing -> notFound
