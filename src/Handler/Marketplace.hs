04881413f5632a16d0505e6f5d1234ac1b450be4
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-11-22 11:57:08 -0700

    rename get parameter

--- src/Handler/Marketplace.hs
+++ src/Handler/Marketplace.hs
@@ -397,11 +397,11 @@ getPackageListR = do
                     sendResponseStatus status400 e
                 Just l -> pure l
         getOsVersionQuery :: Handler (Maybe VersionRange)
-        getOsVersionQuery = lookupGetParam "eos-version-range" >>= \case
+        getOsVersionQuery = lookupGetParam "eos-version-compat" >>= \case
             Nothing  -> pure Nothing
             Just osv -> case Atto.parseOnly parseRange osv of
                 Left _ -> do
-                    let e = InvalidParamsE "get:eos-version-range" osv
+                    let e = InvalidParamsE "get:eos-version-compat" osv
                     $logWarn (show e)
                     sendResponseStatus status400 e
                 Right v -> pure $ Just v
