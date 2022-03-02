74943c7fef179808cc2a9680d4e6c34724b92014
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-03-02 13:51:23 -0700

    add version properly

--- src/Handler/Version.hs
+++ src/Handler/Version.hs
@@ -48,4 +48,4 @@ getVersionWSpec rootDir ext = do
 getSystemStatusR :: Handler OSVersionRes
 getSystemStatusR = do
     -- hardcoded to the next major version release so the UI can by dynamic. this might change depending on the version number we decide to release.
-    pure $ OSVersionRes AVAILABLE $ Version (1, 0, 0, 0)
+    pure $ OSVersionRes AVAILABLE $ Version (0, 3, 0, 0)
