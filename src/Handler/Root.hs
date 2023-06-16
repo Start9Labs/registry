a3efb4e90b74efdf425b6c582f5e581b661d6c1e
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-06-16 12:56:11 -0600

    attempt redirect

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -2,6 +2,7 @@ module Handler.Root where
 import Startlude
 import Yesod
 import Foundation
+import qualified Data.Text as T
 
 getRootR :: HandlerFor RegistryCtx ()
-getRootR =  notImplemented
+getRootR =  redirect $ T.pack "https://marketplace.start9.com"
