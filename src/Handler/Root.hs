550890070ecb282cf1dfc6542ef2cbc8d2c8bc99
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-06-16 13:32:33 -0600

    add query param of hostname

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -3,6 +3,9 @@ import Startlude
 import Yesod
 import Foundation
 import qualified Data.Text as T
+import Settings (AppSettings(registryHostname))
 
 getRootR :: HandlerFor RegistryCtx ()
-getRootR =  redirect $ T.pack "https://marketplace.start9.com"
+getRootR =  do
+  hostname <- getsYesod $ registryHostname . appSettings
+  redirect $ T.pack "https://marketplace.start9.com?api=" <> hostname
