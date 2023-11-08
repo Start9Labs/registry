f98af6cea155eb7e9377b262165d8f03fc8bd54c
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-11-08 10:17:00 -0700

    fix new route setup

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -5,14 +5,15 @@ import Foundation
 import qualified Data.Text as T
 import Settings (AppSettings(registryHostname, marketplaceName))
 import Network.Wai (Request(pathInfo))
+import Lib.Types.Core (PkgId)
 
 getRootR :: HandlerFor RegistryCtx ()
 getRootR =  do
   (url, name) <- getsYesod $ (registryHostname &&& marketplaceName) . appSettings
   redirect $ T.pack "https://marketplace.start9.com?api=" <> url <> T.pack "&name=" <> name
 
-getMarketplaceR :: HandlerFor RegistryCtx ()
-getMarketplaceR = do
+getMarketplaceR :: PkgId -> HandlerFor RegistryCtx ()
+getMarketplaceR _ = do
   (url, name) <- getsYesod $ (registryHostname &&& marketplaceName) . appSettings
   req <- waiRequest
   pathSegments <- pure $ pathInfo req
