2df99d40e96bf3960a0876cde4aa32eb85c61448
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2023-11-08 10:00:09 -0700

    handle redirecting to marketplace paths for brochure

--- src/Handler/Root.hs
+++ src/Handler/Root.hs
@@ -4,8 +4,17 @@ import Yesod
 import Foundation
 import qualified Data.Text as T
 import Settings (AppSettings(registryHostname, marketplaceName))
+import Network.Wai (Request(pathInfo))
 
 getRootR :: HandlerFor RegistryCtx ()
 getRootR =  do
   (url, name) <- getsYesod $ (registryHostname &&& marketplaceName) . appSettings
   redirect $ T.pack "https://marketplace.start9.com?api=" <> url <> T.pack "&name=" <> name
+
+getMarketplaceR :: HandlerFor RegistryCtx ()
+getMarketplaceR = do
+  (url, name) <- getsYesod $ (registryHostname &&& marketplaceName) . appSettings
+  req <- waiRequest
+  pathSegments <- pure $ pathInfo req
+  let pathPiece = "/" <> T.intercalate "/" pathSegments
+  redirect $ T.pack "https://marketplace.start9.com" <> pathPiece <> "?api=" <> url <> T.pack "&name=" <> name
